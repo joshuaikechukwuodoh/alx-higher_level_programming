1daeb4d8f07c89e1a77f6e8bec20ba3ed168d5c9
joshuaikechukwuodoh <odohjoshua82@gmail.com>
2023-04-16 13:10:15 +0000

    task

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,42 +1,44 @@
 #!/usr/bin/python3
-# -*- coding: utf-8 -*-
-
-# Import the required module for interacting with MySQL databases
+# -*- coding: utf-e-8 -*-
+"""
+Created on Sat april  8 09:05:11 2023
+@author: joshua ikechukwu
+"""
 import MySQLdb
+import argparse
 
-# Import the sys module for command-line arguments
-import sys
 
-# Check if the script is being run directly and not being imported as a module
-if __name__ == '__main__':
-    
-    # Retrieve command-line arguments and check that there are exactly 4 arguments
-    args = sys.argv
-    if len(args) != 4:
-        print("Usage: {} username password database_name".format(args[0]))
-        exit(1)
-    
-    # Extract the arguments for the MySQL database connection
-    username = args[1]
-    password = args[2]
-    database_name = args[3]
-    
-    # Connect to the MySQL database using the provided arguments
-    db = MySQLdb.connect(host='localhost', user=username, passwd=password, db=database_name, port=3306)
-    
-    # Create a cursor object that will be used to execute SQL statements
-    cursor = db.cursor()
-    
-    # Execute a SELECT query to retrieve all rows from a table called `states` and order them by their `id` field
-    num_rows = cursor.execute('SELECT * FROM states ORDER BY states.id;')
-    
-    # Fetch all the rows returned by the SELECT query
-    rows = cursor.fetchall()
-    
-    # Loop through each row and print it to the console
+def main(args):
+    # Connect to the database
+    db = MySQLdb.connect(
+        host='localhost',
+        user=args.username,
+        passwd=args.password,
+        db=args.database,
+        port=3306
+    )
+
+    # Execute the query and fetch the results
+    with db.cursor() as cursor:
+        cursor.execute('SELECT * FROM states ORDER BY states.id')
+        rows = cursor.fetchall()
+
+    # Print the results
     for row in rows:
         print(row)
-    
-    # Close the cursor and database connections
+
+    # Close the cursor and database connection
     cursor.close()
     db.close()
+
+
+if __name__ == '__main__':
+    # Parse command-line arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('username', help='MySQL username')
+    parser.add_argument('password', help='MySQL password')
+    parser.add_argument('database', help='MySQL database name')
+    args = parser.parse_args()
+
+    # Call the main function with the parsed arguments
+    main(args)
